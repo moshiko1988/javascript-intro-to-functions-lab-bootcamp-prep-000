985d5a7acba373d0323c46e3fa8d8f4c92718234
Moshe Srur <moshiko1988@gmail.com>
2017-10-23 18:02:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,18 +23,3 @@ function sayHiToGrandma(string){
     return "I love you, too."
   }
 }
-
-// function sayHiToGrandma(string){
-//   console.log(`${string}`);
-//     if (lowercase.toLowerCase() === lowercase) {
-//         return "I can't hear you!";
-//     } else if (uppercase.toUpperCase() === uppercase ) {
-//           return "YES INDEED!";
-//       }else {
-//         return "I love you, too."
-//       }
-// }
-//
-// sayHiToGrandma(uppercase);
-// sayHiToGrandma(lowercase);
-// sayHiToGrandma(other);
