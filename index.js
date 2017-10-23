9ca08e3513380c2333fbdecace0c720a28348148
Moshe Srur <moshiko1988@gmail.com>
2017-10-23 17:49:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ function sayHiToGrandma(string){
     return "I can't hear you!";
   } else if (uppercase.toUpperCase() === uppercase ) {
       return "YES INDEED!";
+    }else {
+      return 
     }
 }
 
