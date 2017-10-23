984b540d95bca7e5dd4caa76546c92366813bc30
Moshe Srur <moshiko1988@gmail.com>
2017-10-23 17:47:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var uppercase = "HELLO!"
 function sayHiToGrandma(string){
   if (lowercase.toLowerCase() === lowercase) {
     return "I can't hear you!";
-  } else (uppercase.toUpperCase() === uppercase ) {
+  } else if (uppercase.toUpperCase() === uppercase ) {
       return "YES INDEED!";
     }
 }
